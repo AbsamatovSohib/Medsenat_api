e59596c98102e711cd92ce08b294d6cb5e7f7133
Absamatov Sohib <sakhibdeveloper2000@gmail.com>
2024-01-29 09:14:16 +0500

    all models created

--- api/models.py
+++ api/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Basemodel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
 
 
-
-class Sponsor(models.Model):
-
+class Sponsor(Basemodel):
 
     title = models.CharField(max_length = 100)
     phone = models.CharField(max_length = 9)
@@ -16,5 +17,55 @@ class Sponsor(models.Model):
         )
     
     person_type = models.CharField(max_length = 10, choices = choices, default = "Jismoniy")
-
     money = models.BigIntegerField()
+
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    situation_choice = (("barchasi","barchasi"),("Yangi","Yangi"),("moderatsiyada","moderiyatsiyada"),("tasdiqlangan","tasdiqlangan"),("bekor qilingan","bekor qilingan"))
+
+    situation = models.CharField(max_length = 10, choices = situation_choice, default = "barchasi")
+
+    def __str__(self):
+        return self.title[0:10]
+    
+    class Meta:
+
+        verbose_name = "Sponsor"
+        verbose_name_plural = "Sponsors"
+
+
+class University(Basemodel):
+
+    title = models.CharField(max_length = 100, unique = True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+
+        verbose_name = "university"
+        verbose_name_plural = "universities"
+
+
+class Student(Basemodel):
+
+    title = models.CharField(max_length = 100)
+    phone = models.CharField(max_length = 12)
+
+
+    student_degree = (
+        ("bachelor","bachelor"),
+        ("master","master"))
+
+    stduent_degree = models.CharField(max_length = 10, choices = student_degree, default = "bachelor")
+
+    contract_price = models.PositiveBigIntegerField()
+
+
+    def __str__(self) :
+        return self.title
+    
+    class Meta:
+
+        verbose_name = "Student"
+        verbose_name_plural = "Students"
